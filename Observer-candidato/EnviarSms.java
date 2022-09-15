06bd716613f1164b1916c0b5bbe33c4b623c0f5c
DanielRodrigues87 <67766681+DanielRodrigues87@users.noreply.github.com>
2022-09-15 11:34:10 -0300

    Primeiro commit

---
+++ Observer-candidato/EnviarSms.java
@@ -0,0 +1,11 @@
+public class EnviarSms extends CandidatoObserver{
+    
+    public EnviarSms(CandidatoDominio candidato) {
+        super(candidato);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Enviando SMS");
+    }
+}
